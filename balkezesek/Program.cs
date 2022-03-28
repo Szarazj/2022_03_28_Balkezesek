f7c0074e0bc3da66c5ace3c7f3bb4b25787b5d61
szrz_jzsf44 <szarazj.12d@acsjszki.hu>
2022-03-28 13:13:29 +0200

    3 feladat

--- balkezesek/Program.cs
+++ balkezesek/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace balkezesek
 {
@@ -10,6 +11,17 @@ namespace balkezesek
     {
         static void Main(string[] args)
         {
+            List<sportoló> sportolók = new List<sportoló>();
+            foreach (var sor in File.ReadAllLines("balkezesek.csv").Skip(1))
+            {
+                sportolók.Add(new sportoló(sor));
+            }
+
+
+            //3. feladat
+            Console.WriteLine($"3. feladat: {sportolók.Count}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ balkezesek/sportoló.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace balkezesek
+{
+    class sportoló
+    {
+        public  string Név { get; set; }
+        public string Első { get; set; }
+        public string Utolsó { get; set; }
+        public int Súly { get; set; }
+        public int Magasság { get; set; }
+
+        public sportoló (string sor)
+        {
+            string[] t = sor.Split(';');
+            Név = t[0];
+            Első = t[1];
+            Utolsó = t[2];
+            Súly = int.Parse(t[3]);
+            Magasság = int.Parse(t[4]);
+
+        }
+    }
+}
